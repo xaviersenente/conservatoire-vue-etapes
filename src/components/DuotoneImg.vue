587b021a22c0ae1c0dcd30849d9560e264d8ca67
Xavier SENENTE <xavier.senente@univ-fcomte.fr>
2023-04-27 11:42:29 +0200

    hero end

--- src/components/DuotoneImg.vue
+++ src/components/DuotoneImg.vue
@@ -2,15 +2,19 @@
 defineProps<{
   imgAlt: string
   imgPath: string
+  classPicture?: string
+  classImg?: string
 }>()
 </script>
 
 <template>
   <picture
     class="relative flex overflow-hidden bg-pink before:absolute before:inset-0 before:z-[1] before:bg-indigo before:mix-blend-lighten before:transition-all before:duration-300 before:ease-in-out"
+    :class="classPicture"
   >
     <img
       class="flex-[1_0_100%] object-cover mix-blend-multiply grayscale transition-all duration-300 ease-in-out"
+      :class="classImg"
       :src="imgPath"
       :alt="imgAlt"
     />
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -9,15 +9,23 @@ defineProps<{
 }>()
 </script>
 <template>
-  <div>
-    <div>
+  <div class="grille relative grid h-screen grid-rows-[4fr_1fr] lg:grid-rows-[3fr_1fr]">
+    <div class="col-span-9 my-12 space-y-8 self-end text-white">
       <h1>{{ title }}</h1>
-      <h2 v-if="subTitle">{{ subTitle }}</h2>
+      <h2 v-if="subTitle" class="font-sans text-sm font-bold uppercase">
+        {{ subTitle }}
+      </h2>
     </div>
 
-    <div>
+    <div
+      class="relative col-span-9 row-start-2 -mx-6 flex items-center bg-white px-6 lg:mx-0 lg:pl-0 lg:before:absolute lg:before:top-0 lg:before:ml-[-100%] lg:before:h-full lg:before:w-full lg:before:bg-white"
+    >
       <slot name="chapo"></slot>
     </div>
-    <DuotoneImg :imgPath="imgPath" :imgAlt="imgAlt" />
+    <DuotoneImg
+      :imgPath="imgPath"
+      :imgAlt="imgAlt"
+      classPicture="!absolute inset-0 object-cover z-[-1]"
+    />
   </div>
 </template>
