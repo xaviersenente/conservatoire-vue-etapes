32d6729f964caf6606c1b2fbe2991913daabf033
Xavier SENENTE <xavier.senente@univ-fcomte.fr>
2023-04-27 09:54:36 +0200

    hero structure

---
+++ src/components/Hero.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+  imgPath: string
+  imgAlt: string
+  subTitle?: string
+}>()
+</script>
+<template>
+  <div>
+    <div>
+      <h1>{{ title }}</h1>
+      <h2 v-if="subTitle">{{ subTitle }}</h2>
+    </div>
+
+    <div>
+      <slot name="chapo"></slot>
+    </div>
+    <img :src="imgPath" :alt="imgAlt" />
+  </div>
+</template>
